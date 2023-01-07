db24c4476afdd41ea97a563f0bbb0d3c9dac48c0
Git-CAK <cak20021004@163.com>
2023-01-07 20:44:23 +0800

    bug1

--- src/main/java/com/myGit/GitTest.java
+++ src/main/java/com/myGit/GitTest.java
@@ -3,5 +3,6 @@ package com.myGit;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git3");
+        System.out.println("hello bug");
     }
 }
