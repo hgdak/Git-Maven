180947f0df65c382a5a8cefa630482dd4b976bb1
Git-CAK <cak20021004@163.com>
2023-01-15 17:35:20 +0800

    hrlkl

--- src/main/java/com/myGit/GitTest.java
+++ src/main/java/com/myGit/GitTest.java
@@ -3,6 +3,10 @@ package com.myGit;
 
 public class GitTest {
     public static void main(String[] args) {
+        System.out.println("hello Git1");
+        System.out.println("hello Git2");
         System.out.println("hello Git3");
+        System.out.println("hello Git4");
+        System.out.println("hello Git5");
     }
 }
