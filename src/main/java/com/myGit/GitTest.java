19d251beb568d4db6d957e428895ee23468da182
Git-CAK <cak20021004@163.com>
2023-01-15 17:28:09 +0800

    gggg

--- src/main/java/com/myGit/GitTest.java
+++ src/main/java/com/myGit/GitTest.java
@@ -4,12 +4,5 @@ package com.myGit;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git3");
-        System.out.println("hello bug");
-        System.out.println("冲突111");
-        System.out.println("冲突222");
-        System.out.println("pushTest");
-        System.out.println("pushTestSSH");
-        System.out.println("pullTest");
-        System.out.println("GiteeTest");
     }
 }
